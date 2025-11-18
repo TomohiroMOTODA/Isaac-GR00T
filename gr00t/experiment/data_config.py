269865816e258ca1d8ddb482840baa82de66e00f
TomohiroMOTODA <motoda.populus14144@gmail.com>
2025-11-18 19:40:45 +0900

    Add AlohaSim and AlohaReal data configuration classes with video and state transformations

--- gr00t/experiment/data_config.py
+++ gr00t/experiment/data_config.py
@@ -147,6 +147,116 @@ def load_data_config(data_config_str: str) -> BaseDataConfig:
 ###########################################################################################
 
 
+
+class AlohaSimDataConfig(BaseDataConfig):
+    video_keys = ["video.cam_top",
+                  "video.cam_left_wrist",
+                  "video.cam_right_wrist"]
+    state_keys = ["state.qpos", 
+                  "state.qvel", 
+                  "state.effort"]
+    action_keys = ["action.qpos"]
+    language_keys = []
+    observation_indices = [0]
+    action_indices = list(range(16))
+
+    def transform(self) -> ModalityTransform:
+        transforms = [
+            # video transforms
+            VideoToTensor(apply_to=self.video_keys),
+            VideoCrop(apply_to=self.video_keys, scale=0.95),
+            VideoResize(apply_to=self.video_keys, height=224, width=224, interpolation="linear"),
+            VideoColorJitter(
+                apply_to=self.video_keys,
+                brightness=0.3,
+                contrast=0.4,
+                saturation=0.5,
+                hue=0.08,
+            ),
+            VideoToNumpy(apply_to=self.video_keys),
+            # state transforms
+            StateActionToTensor(apply_to=self.state_keys),
+            StateActionTransform(
+                apply_to=self.state_keys,
+                normalization_modes={key: "min_max" for key in self.state_keys},
+            ),
+            # action transforms
+            StateActionToTensor(apply_to=self.action_keys),
+            StateActionTransform(
+                apply_to=self.action_keys,
+                normalization_modes={key: "min_max" for key in self.action_keys},
+            ),
+            # concat transforms
+            ConcatTransform(
+                video_concat_order=self.video_keys,
+                state_concat_order=self.state_keys,
+                action_concat_order=self.action_keys,
+            ),
+            # model-specific transform
+            GR00TTransform(
+                state_horizon=len(self.observation_indices),
+                action_horizon=len(self.action_indices),
+                max_state_dim=64,
+                max_action_dim=32,
+            ),
+        ]
+        return ComposedModalityTransform(transforms=transforms)
+    
+class AlohaRealDataConfig(BaseDataConfig):
+    video_keys = ["video.cam_high",
+                  "video.cam_low",
+                  "video.cam_left_wrist",
+                  "video.cam_right_wrist"]
+    state_keys = ["state.qpos", 
+                  "state.qvel", 
+                  "state.effort"]
+    action_keys = ["action.qpos"]
+    language_keys = []
+    observation_indices = [0]
+    action_indices = list(range(16))
+
+    def transform(self) -> ModalityTransform:
+        transforms = [
+            # video transforms
+            VideoToTensor(apply_to=self.video_keys),
+            VideoCrop(apply_to=self.video_keys, scale=0.95),
+            VideoResize(apply_to=self.video_keys, height=224, width=224, interpolation="linear"),
+            VideoColorJitter(
+                apply_to=self.video_keys,
+                brightness=0.3,
+                contrast=0.4,
+                saturation=0.5,
+                hue=0.08,
+            ),
+            VideoToNumpy(apply_to=self.video_keys),
+            # state transforms
+            StateActionToTensor(apply_to=self.state_keys),
+            StateActionTransform(
+                apply_to=self.state_keys,
+                normalization_modes={key: "min_max" for key in self.state_keys},
+            ),
+            # action transforms
+            StateActionToTensor(apply_to=self.action_keys),
+            StateActionTransform(
+                apply_to=self.action_keys,
+                normalization_modes={key: "min_max" for key in self.action_keys},
+            ),
+            # concat transforms
+            ConcatTransform(
+                video_concat_order=self.video_keys,
+                state_concat_order=self.state_keys,
+                action_concat_order=self.action_keys,
+            ),
+            # model-specific transform
+            GR00TTransform(
+                state_horizon=len(self.observation_indices),
+                action_horizon=len(self.action_indices),
+                max_state_dim=64,
+                max_action_dim=32,
+            ),
+        ]
+        return ComposedModalityTransform(transforms=transforms)
+
 class FourierGr1ArmsOnlyDataConfig(BaseDataConfig):
     video_keys = ["video.ego_view"]
     state_keys = [
@@ -773,6 +883,8 @@ class AgibotGenie1DataConfig(BaseDataConfig):
 ###########################################################################################
 
 DATA_CONFIG_MAP = {
+    "aloha_sim": AlohaSimDataConfig(),
+    "aloha_real": AlohaRealDataConfig(),
     "fourier_gr1_arms_waist": FourierGr1ArmsWaistDataConfig(),
     "fourier_gr1_arms_only": FourierGr1ArmsOnlyDataConfig(),
     "fourier_gr1_full_upper_body": FourierGr1FullUpperBodyDataConfig(),
